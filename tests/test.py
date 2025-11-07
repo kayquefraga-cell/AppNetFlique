dc354e74f3cb16de09de99e58968dae4491c7777
kayque <kayquefraga@ucl.br>
2025-11-07 18:53:41 -0300

    add função

--- tests/test.py
+++ tests/test.py
@@ -3,18 +3,18 @@ from FilaFilmes import filaFilmes
 def testar_fila_vazia():
     return filaFilmes.empty()
 def testar_adicionar_filme(filme):
-    filaFilmes.put(filme)
-    return filaFilmes.qsize()
+     filaFilmes.put(filme)
+     return filaFilmes.qsize()
 def testar_listar_filmes():
     filmes = []
     tamanho_fila = filaFilmes.qsize()
     
     for _ in range(tamanho_fila):
-        filme = filaFilmes.get()
-        filmes.append(filme)
-        filaFilmes.put(filme)  # Reinsere o filme na fila
+       filme = filaFilmes.get()
+       filmes.append(filme)
+       filaFilmes.put(filme)  # Reinsere o filme na fila
     
-    return filmes
+       return filmes
 def testar_pesquisar_filme(titulo):
     resultados = []
     tamanho_fila = filaFilmes.qsize()
@@ -22,7 +22,7 @@ def testar_pesquisar_filme(titulo):
     for _ in range(tamanho_fila):
         filme = filaFilmes.get()
         if titulo.lower() in filme['titulo'].lower():
-            resultados.append(filme)
+           resultados.append(filme)
         filaFilmes.put(filme)  # Reinsere o filme na fila
     
     return resultados
